fbbf24bb9827c52855b61c6799f3eaabfa3531e6
Andrés Montañez <andresmontanez@gmail.com>
2021-05-22 01:22:01 -0300

    Updates

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ try {
     // `who-to-greet` input defined in action metadata file
     const labels = JSON.parse(core.getInput('labels'));
     console.log(`Labels`, labels);
+    labels.forEach(label => {
+        console.log(`Labels`, label);
+    });
+
+    const octokit = github.getOctokit(core.getInput('github-token'));
+    const existingLabels = await octokit.paginate(octokit.issues.listLabelsForRepo, {
+        ...github.context.repo
+    }).map(label => {
+        return {
+            name: label.name,
+            color: label.color,
+            description: label.description || ''
+        };
+    });
+    console.log(existingLabels);
 
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
