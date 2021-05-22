d11ebb7b57520645154ac14f933395c9a51fd6a6
Andrés Montañez <andresmontanez@gmail.com>
2021-05-22 13:23:17 -0300

    Updates

--- index.js
+++ index.js
@@ -3,17 +3,31 @@ const github = require('@actions/github');
 
 async function run() {
     try {
-        const labels = JSON.parse(core.getInput('labels'));
-        labels.forEach(label => {
-            console.info('Label data', label);
-        });
-
+        // Get current exiting labels from the repository
         const octokit = github.getOctokit(core.getInput('github-token'));
-        const existingLabels = await getRepoLabels(octokit, github);
+        let existingLabels = await getRepoLabels(octokit, github);
         console.log(existingLabels);
 
+        // Labels to be set on the repository
+        const desiredLabels = JSON.parse(core.getInput('labels'));
+        desiredLabels.forEach(label => {
+            if (existingLabels[label.name]) {
+                if (
+                    existingLabels[label.name].color !== label.color ||
+                    existingLabels[label.name].description !== label.description
+                ) {
+                    console.log(`Updating label ${label.name}`);
+                    let response = updateLabel(octokit, github, label);
+                }
+            } else {
+                console.log(`Creating label ${label.name}`);
+                let response = createLabel(octokit, github, label);
+            }
+        });
+        console.log(desiredLabels);
+
         // Get the JSON webhook payload for the event that triggered the workflow
-        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        //const payload = JSON.stringify(github.context.payload, undefined, 2)
         //console.log(`The event payload: ${payload}`);
     } catch (error) {
         core.setFailed(error.message);
@@ -22,16 +36,40 @@ async function run() {
 
 async function getRepoLabels(octokit, github) {
     let response = await octokit.rest.issues.listLabelsForRepo(github.context.repo);
-    let labels = [];
+    let labels = {};
     response.data.forEach(label => {
-        labels.push({
+        labels[label.name] = {
             name: label.name,
             color: label.color,
             description: label.description
-        });
+        };
     });
 
     return labels;
+}
+
+async function updateLabel(octokit, github, label) {
+    let payload = {
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        name: label.name,
+        color: label.color,
+        description: label.description || '',
+    };
+
+    return await octokit.rest.issues.updateLabel(payload);
+}
+
+async function createLabel(octokit, github, label) {
+    let payload = {
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        name: label.name,
+        color: label.color,
+        description: label.description || '',
+    };
+
+    return await octokit.rest.issues.createLabel(payload);
 }
 
 run();
